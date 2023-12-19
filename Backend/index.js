a19e0b2755ce86640ca9fc43a81a802ea8e91575
BilalMalik08 <muhammad.billal.cs@gmail.com>
2023-12-19 14:41:36 +0500

    eighth commit

--- Backend/index.js
+++ Backend/index.js
@@ -18,12 +18,7 @@ dotenv.config();
 
 // Use CORS middleware in your Express app
 console.log("Applying CORS middleware...");
-app.use(
-  cors({
-    origin: "https://fast-food-app-rose.vercel.app",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
