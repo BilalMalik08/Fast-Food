3ee0406b60924e1c8fc0cdd11aabc3ba84e946ef
BilalMalik08 <muhammad.billal.cs@gmail.com>
2023-12-22 10:07:49 +0500

    url changed

--- Frontend/src/components/MenuCard.js
+++ Frontend/src/components/MenuCard.js
@@ -30,7 +30,7 @@ function MenuCard() {
                 <div className="card menu-card">
                   <img
                     className="menu-card-img"
-                    src={`/nmenu/${menuItem.image}`}
+                    src={`${apiURL}/menu/uploads/${menuItem.image}`}
                     alt={menuItem.adminCategory}
                   />
                   <div className="card-body menu-card-body">
